07e694408812b1233351d18e3abb52f4f51a1fb3
Vojtech Ruzicka <vojtech.ruzicka@merck.com>
2017-04-25 18:35:30 +0200

    Added another bonus task

---
+++ bonus/02_raise_exception.py
@@ -0,0 +1,4 @@
+#Napis funkci s jednim vstupnim parametrem, ktera vyhodi vyjimku, pokud na vstupu
+# dostane retezec delsi nez 10 znaku nebo retezec,
+# ktery konci 'XXX'.
+# Napis test k teto funkci.
